55593c58b29a353148bd8b15cc2b6584213b5688
Alexis <alexis@gmail.com>
2026-04-03 11:32:59 +0200

    nuevos cambios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import "./index.css";
-import "./App.css";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
