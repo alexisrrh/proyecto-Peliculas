fb3f3dd34ea1db436990d964b938554144a27efb
Danieldaviddf <danieldaviddf@hotmail.com>
2026-04-15 18:47:56 +0200

    Creacion de componente categoria reutilizable

--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Favoritos from "../pages/Favoritos";
 import Animadas from "../pages/05-Animadas";
-import Populares from "../pages/01-Populares";
 import Accion1 from "../pages/02-Accion";
 import Terror from "../pages/03-Terror";
 import Comedia from "../pages/04-Comedia";
 import Modal from "../componentes/Modal";
 import Search from "../pages/Busqueda";
+import Categorias from "../pages/Categorias";
+
+
 export const AppRoutes = () => {
   return (
     <Routes>
@@ -15,11 +17,11 @@ export const AppRoutes = () => {
       <Route path="/favoritos" element={<Favoritos />} />
       
   
-      <Route path="/populares" element={<Populares />} />
-      <Route path="/accion" element={<Accion1 />} />
-      <Route path="/terror" element={<Terror />} />
-      <Route path="/comedia" element={<Comedia />} />
-      <Route path="/animadas" element={<Animadas />} />
+      <Route path="/populares" element={<Categorias />} />
+      <Route path="/accion" element={<Categorias />} />
+      <Route path="/terror" element={<Categorias />} />
+      <Route path="/comedia" element={<Categorias />} />
+      <Route path="/animadas" element={<Categorias />} />
       <Route path="/modal/:id" element={<Modal/>} />
       <Route path="/search/:texto" element={<Search />} />
    
