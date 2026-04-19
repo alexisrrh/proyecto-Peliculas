31a12d43089e9305e80f741b62fa794c125b8767
Danieldaviddf <danieldaviddf@hotmail.com>
2026-04-19 11:05:30 +0200

    actualizacion enlaces en footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -15,22 +15,47 @@ const Footer = () => {
         {/* Copyright */}
         <div className="text-sm">
           © {new Date().getFullYear()} VHSFlix. Todos los derechos reservados.
+
         </div>
 
-        {/* Redes Sociales */}
-        <div className="flex items-center gap-6">
-          <a href="#" className="hover:text-orange-300 transition-transform hover:scale-125">
-            <FaInstagram size={22} />
-          </a>
-          <a href="#" className="hover:text-orange-300 transition-transform hover:scale-125">
-            <FaFacebook size={22} />
-          </a>
-          <a href="#" className="hover:text-orange-300 transition-transform hover:scale-125">
-            <FaWhatsapp size={22} />
-          </a>
+        
+          {/* Bloque Persona 1 */}
+          <div className="flex flex-col items-center gap-2">
+            <div className="text-sm font-medium">
+              Alexis Rafael Rodriguez Haddad
+            </div>
+            <div className="flex items-center gap-4">
+              <a href="https://instagram.com/alexisrrh" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <FaInstagram size={22} />
+              </a>
+              <a href="https://wa.me/34674516605?text=Hola%20quiero%20información" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <FaWhatsapp size={22} />
+              </a>
+              <a href="https://github.com/alexisrrh" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <i className="text-lg fa-brands fa-github"></i>
+              </a>
+            </div>
+          </div>
+
+          {/* Bloque Persona 2 */}
+          <div className="flex flex-col items-center gap-2">
+            <div className="text-sm font-medium">
+              Daniel David Díaz Flores
+            </div>
+            <div className="flex items-center gap-4">
+              <a href="https://instagram.com/danieldaviddf" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <FaInstagram size={22} />
+              </a>
+              <a href="https://wa.me/34643804335?text=Hola%20quiero%20información" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <FaWhatsapp size={22} />
+              </a>
+              <a href="https://github.com/Danieldaviddf" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <i class="text-lg fa-brands fa-github"></i>
+              </a>
+            </div>
+          </div>
         </div>
 
-      </div>
     </footer>
   );
 };
