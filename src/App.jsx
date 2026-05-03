c43c9ec1e8a2837818cc0307680d4aba51456ca9
Danieldaviddf <danieldaviddf@hotmail.com>
2026-05-03 16:54:33 +0200

    Actualizacion fondo estrellado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
-import  Navbar  from "./componentes/Navbar";
-import  Footer  from "./componentes/Footer";
+import Navbar from "./componentes/Navbar";
+import Footer from "./componentes/Footer";
+import StarBackground from "./componentes/StarBackground"; // <--- Importamos el fondo
 import { AppRoutes } from "./routes/appRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
+      {/* Añadimos 'relative' para que el z-index de los hijos funcione bien */}
+      <div className="flex flex-col min-h-screen relative text-white">
+        
+        {/* 1. EL FONDO (Siempre detrás) */}
+        <StarBackground />
+      
+        {/* 2. EL CONTENIDO (Encima del fondo) */}
         <Navbar />
       
-        <main className="flex-grow">
+        <main className="flex-grow relative z-10">
           <AppRoutes />
         </main>
+
         <Footer />
       </div>
     </BrowserRouter>
@@ -19,4 +27,3 @@ function App() {
 }
 
 export default App;
-
