b9595afc9decb7f358cf3c5e436376485f205609
Danieldaviddf <danieldaviddf@hotmail.com>
2026-05-25 10:32:12 +0200

    cambios models

--- Back/models.py
+++ Back/models.py
@@ -15,20 +15,23 @@ class User(db.Model):
     favoritos: Mapped[List["Favorito"]] = relationship("Favorito", back_populates="user")
     avatar: Mapped[str] = mapped_column(String(500), nullable=True, default="")
 
-    def serialize(self):
-        default_avatar = "https://i.pinimg.com/736x/c5/77/35/c577359e3223df4b3d92e785bf7464a8.jpg"
-        # Si no hay avatar, devolvemos un avatar generado por su email (siempre funciona)
+def serialize(self):
+    default_avatar = "https://i.pinimg.com/736x/c5/77/35/c577359e3223df4b3d92e785bf7464a8.jpg"
+    
+    # Si el avatar es None, o está vacío, o tiene el link roto base:
+    if not self.avatar or self.avatar == "" or self.avatar == "https://i.pinimg.com/736x/1e/e8/8a/1ee88a173e942bada798a19fd02f715f.jpg":
+        img_final = default_avatar
+    else:
+        # Si tiene cualquier otra cosa (Avatar 2, 3, 4 o cualquier URL), la respetamos
         img_final = self.avatar
-        if not img_final or img_final == "" or "https://i.pinimg.com/736x/c5/77/35/c577359e3223df4b3d92e785bf7464a8.jpg" in img_final:
-            img_final = default_avatar
 
-        return {
-            "id": self.id,
-            "nombre": self.nombre,
-            "apellido":self.apellido,
-            "email": self.email,
-            "avatar": img_final
-        }
+    return {
+        "id": self.id,
+        "nombre": self.nombre,
+        "apellido": self.apellido,
+        "email": self.email,
+        "avatar": img_final
+    }
 
 
 # PELICULA
